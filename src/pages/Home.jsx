3f4b3e486f2ed4158b9717fdef66e25442242d65
Ibrahim aboubacar cisse <ibrahimaboubacarcisse@gmail.com>
2024-07-25 18:52:23 +0000

    fixe text

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,9 +7,9 @@ function Home() {
             <HeroSection />
             <MyWork />
             <div className="relative">
-                <div className="relative z-10 mx-auto max-w-screen-xl px-4">
+                {/* <div className="relative z-10 mx-auto max-w-screen-xl px-4">
                     <h1>Home</h1>
-                </div>
+                </div> */}
                 <div className="absolute inset-0 z-0 grid grid-cols-2">
                     <div className="bg-gradient-to-r from-orange-200 to-violet-200/0"></div>
                     <div className="bg-gradient-to-r from-orange-100/0 to-violet-200"></div>
